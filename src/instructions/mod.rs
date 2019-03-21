dff4ef9eeb57c33852ca4ff22f95b551f1a15e7d
Jiajie Chen <noc@jiegec.ac.cn>
2019-03-21 10:22:18 +0800

    Add comments for wait() and nop()

--- src/instructions/mod.rs
+++ src/instructions/mod.rs
@@ -1,9 +1,13 @@
 //! MIPS specific instructions
 
+/// Purpose: Enter Standby Mode
+/// Wait for Event
 pub unsafe fn wait() {
     asm!("wait" : : : : "volatile");
 }
 
+/// Purpose: No Operation
+/// To perform no operation.
 pub unsafe fn nop() {
     asm!("nop" : : : : "volatile");
 }
