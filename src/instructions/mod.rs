0b59508660d7b1b0d5dd6e4b987de2ca07955611
Yuhao Zhou <miskcoo@gmail.com>
2019-03-28 01:39:52 +0800

    Add some special instructions.

--- src/instructions/mod.rs
+++ src/instructions/mod.rs
@@ -1,13 +1,37 @@
 //! MIPS specific instructions
 
-/// Purpose: Enter Standby Mode
-/// Wait for Event
 pub unsafe fn wait() {
     asm!("wait" : : : : "volatile");
 }
 
-/// Purpose: No Operation
-/// To perform no operation.
 pub unsafe fn nop() {
     asm!("nop" : : : : "volatile");
+}
+
+pub unsafe fn tlbr() {
+    asm!("tlbr" : : : : "volatile");
+}
+
+pub unsafe fn tlbp() {
+    asm!("tlbp" : : : : "volatile");
+}
+
+pub unsafe fn tlbwr() {
+    asm!("tlbwr" : : : : "volatile");
+}
+
+pub unsafe fn tlbwi() {
+    asm!("tlbwi" : : : : "volatile");
+}
+
+pub unsafe fn syscall() {
+    asm!("syscall" : : : : "volatile");
+}
+
+pub unsafe fn breakpoint() {
+    asm!("break" : : : : "volatile");
+}
+
+pub unsafe fn exception_return() {
+    asm!("eret" : : : : "volatile");
 }
