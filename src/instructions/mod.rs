3fb8a25e7947a35ec9171826c4d9c17716633a2f
Harry Chen <i@harrychen.xyz>
2019-03-28 14:08:20 +0800

    Extract instruction invocation functions to a macro
    Signed-off-by: Harry Chen <i@harrychen.xyz>

--- src/instructions/mod.rs
+++ src/instructions/mod.rs
@@ -1,37 +1,28 @@
 //! MIPS specific instructions
 
-pub unsafe fn wait() {
-    asm!("wait" : : : : "volatile");
+macro_rules! define_instruction {
+    // specify a different function name
+    ($inst: expr, $fun: ident) => {
+        #[doc = "invoke `"]
+        #[doc = $inst]
+        #[doc = "` instruction"]
+        pub unsafe fn $fun() {
+            asm!($inst : : : : "volatile");
+        }
+    };
+
+    // directly use instruction name as function name
+    ($inst: ident) => {
+        define_instruction!(stringify!($inst), $inst);
+    }
 }
 
-pub unsafe fn nop() {
-    asm!("nop" : : : : "volatile");
-}
-
-pub unsafe fn tlbr() {
-    asm!("tlbr" : : : : "volatile");
-}
-
-pub unsafe fn tlbp() {
-    asm!("tlbp" : : : : "volatile");
-}
-
-pub unsafe fn tlbwr() {
-    asm!("tlbwr" : : : : "volatile");
-}
-
-pub unsafe fn tlbwi() {
-    asm!("tlbwi" : : : : "volatile");
-}
-
-pub unsafe fn syscall() {
-    asm!("syscall" : : : : "volatile");
-}
-
-pub unsafe fn breakpoint() {
-    asm!("break" : : : : "volatile");
-}
-
-pub unsafe fn exception_return() {
-    asm!("eret" : : : : "volatile");
-}
+define_instruction!(wait);
+define_instruction!(nop);
+define_instruction!(tlbr);
+define_instruction!(tlbp);
+define_instruction!(tlbwr);
+define_instruction!(tlbwi);
+define_instruction!(syscall);
+define_instruction!("break", breakpoint);
+define_instruction!("eret", exception_return);
